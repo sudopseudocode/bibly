a4b5f5db8c0431c814462ee9b98b53e77f57ef47
Paul DiLoreto <paul.diloreto@gmail.com>
2019-06-16 22:45:38 -0700

    Somewhat working webpack local workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  title: 'Bibly',
+  template: './src/index.html',
+  filename: './index.html',
+});
+
+const hotModulePlugin = new webpack.HotModuleReplacementPlugin();
+
 module.exports = {
-  externals: {
-    fs: require("fs")
-  }
+  entry: ['webpack/hot/dev-server', './src/index.js'],
+  target: 'electron-renderer',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: './',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/, /.json?/],
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [htmlPlugin, hotModulePlugin],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    publicPath: 'http://localhost:9000',
+    contentBase: path.join(__dirname, 'assets'),
+    open: false,
+    lazy: false,
+    compress: true,
+    historyApiFallback: true,
+    port: 9000,
+  },
 };
