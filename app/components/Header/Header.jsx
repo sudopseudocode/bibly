e6ce362eb247215b2e3e46df3091f17511f511fd
Paul DiLoreto <paul.diloreto@gmail.com>
2019-08-19 23:59:18 -0700

    Made header position sticky

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
 import SearchControls from './SearchControls';
 import ViewControls from './ViewControls';
 
+const useStyles = makeStyles(theme => ({
+  container: {
+    position: 'sticky',
+    top: 0,
+    backgroundColor: theme.palette.background.default,
+  },
+}));
+
 const Header = (props) => {
   const { topHeight } = props;
+  const classes = useStyles();
 
   return (
-    <React.Fragment>
+    <div className={classes.container}>
       <SearchControls topHeight={topHeight} />
       <ViewControls />
-    </React.Fragment>
+    </div>
   );
 };
 
