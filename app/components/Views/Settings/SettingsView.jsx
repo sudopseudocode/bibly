a0e4f42795a8588af5e52a7c55280af7dee469ad
Paul DiLoreto <paul.diloreto@gmail.com>
2019-08-20 01:54:11 -0700

    Removed react-color and Theme settings

--- app/components/Views/Settings/SettingsView.jsx
+++ app/components/Views/Settings/SettingsView.jsx
@@ -2,12 +2,11 @@ import React, { useState } from 'react';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import General from './General';
-import Theme from './Theme';
 
 const views = [
   { label: 'General', Component: General },
   { label: 'Conversion' },
-  { label: 'Theme', Component: Theme },
+  { label: 'Extras' },
 ];
 
 const SettingsView = () => {
