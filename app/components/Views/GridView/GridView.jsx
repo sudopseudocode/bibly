b4ad07401f126d1d5d519797fb375bfc1849e13f
Paul DiLoreto <paul.diloreto@gmail.com>
2019-09-07 13:20:42 -0700

    Virtualized grid working better

--- app/components/Views/GridView/GridView.jsx
+++ app/components/Views/GridView/GridView.jsx
@@ -1,82 +1,69 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/styles';
 import {
   AutoSizer,
   CellMeasurerCache,
   CellMeasurer,
-	List,
+  Grid,
 } from 'react-virtualized';
 import GridItem from './GridItem';
 
-const useStyles = makeStyles({
-  row: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-end',
-  },
-});
-
 const cache = new CellMeasurerCache({
-  defaultHeight: 200,
-  fixedWidth: true
+  defaultHeight: 280,
+  defaultWidth: 200,
 });
 
 const GridView = (props) => {
   const { data } = props;
-  const classes = useStyles();
   const booksPerRow = 4;
-  const rowRenderer = ({ key, style, index, parent }) => {
-    const startIndex = index * booksPerRow;
-    const books = data.slice(
-      startIndex,
-      (startIndex) + booksPerRow,
-    );
+  const cellRenderer = ({ key, style, columnIndex, rowIndex, parent }) => {
+    const startIndex = rowIndex * booksPerRow;
+    const book = data[startIndex + columnIndex];
+    if (!book) return null;
+
     return (
       <CellMeasurer
         cache={cache}
-        columnIndex={0}
         key={key}
         overscanRowCount={10}
         parent={parent}
-        rowIndex={index}
+        rowIndex={rowIndex}
+        columnIndex={columnIndex}
       >
-        <div key={key} style={style} className={classes.row}>
-          {books.map(book => (
-            <GridItem
-              key={book.id}
-              title={book.title}
-              author={book.author}
-              fileFormat="epub"
-              bookCover={book.bookCover}
-            />
-          ))}
+        <div style={style}>
+          <GridItem
+            key={book.id}
+            title={book.title}
+            author={book.author}
+            fileFormat="epub"
+            bookCover={book.bookCover}
+          />
         </div>
       </CellMeasurer>
     );
   };
-  rowRenderer.propTypes = {
+  cellRenderer.propTypes = {
     key: PropTypes.string.isRequired,
     style: PropTypes.shape({}).isRequired,
-    index: PropTypes.number.isRequired,
+    columnIndex: PropTypes.number.isRequired,
+    rowIndex: PropTypes.number.isRequired,
     parent: PropTypes.element.isRequired,
   };
 
   return (
     <AutoSizer>
-      {({ height, width }) => {
-          return (
-            <List
-              style={{ overflow: 'auto' }}
-              height={height}
-              width={width}
-              rowCount={data.length / booksPerRow}
-              rowHeight={cache.rowHeight}
-              rowRenderer={rowRenderer}
-            />
-          );
-        }}
+      {({ height, width }) => (
+        <Grid
+          style={{ overflow: 'auto' }}
+          cellRenderer={cellRenderer}
+          columnCount={booksPerRow}
+          columnWidth={cache.columnWidth}
+          rowCount={Math.ceil(data.length / booksPerRow)}
+          rowHeight={cache.rowHeight}
+          height={height}
+          width={width}
+        />
+      )}
     </AutoSizer>
   );
 };
